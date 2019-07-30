d7ac74e89156183211fef282f4549c829d953017
Hisahiro Tsukamoto <hishats@gmail.com>
2019-07-30 09:14:55 +0900

    Minor amendment

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM hihats/awscli:alpine
 
-RUN apk --update add  --no-cache --update-cache wget libxml2-utils && \
-    rm -rf /var/cache/apk/*
+RUN apk --update add --no-cache --update-cache wget ca-certificates openssl libxml2-utils \
+    && update-ca-certificates \
+    && rm -rf /var/cache/apk/*
 
 COPY ./entrypoint.sh /
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -8,19 +8,26 @@ then
     exit 1
 fi
 ORIGIN_URL=$1
+SITEMAP_URL=http://${ORIGIN_URL}/sitemap-pt-post-`date +%Y-%m`.xml
 
-STATUS=$(curl -s ${ORIGIN_URL} -o /dev/null -w '%{http_code}\n')
+STATUS=$(curl -s $SITEMAP_URL -o /dev/null -w '%{http_code}\n')
 if [ "$STATUS" = "200" ]
 then
-    wget --quiet http://${ORIGIN_URL}/sitemap-pt-post-`date +%Y-%m`.xml \ --output-document - \
+    wget --quiet $SITEMAP_URL -O - \
       | grep "<loc>" \
       | egrep -o "https?://[^<]+" \
       | xargs -I{} echo "{} {}" \
-      | sed -e s#${ORIGIN_URL}## \
-      | xargs -n 2 sh -c 'wget --no-parent -B ${ORIGIN_URL} -I $0 -p $1'
+      | sed -e s#http://${ORIGIN_URL}/## \
+      | xargs -n 2 sh -c 'wget --no-parent -B ${ORIGIN_URL} -I $0 -p $1' && ls -la
 fi
 
 wget -r -l2 --page-requisites -q http://${ORIGIN_URL}
+
+if [ "$S3_BUCKET" = "" ]
+then
+    echo "[ERROR]: environmental valuable error for S3"
+    exit 1
+fi
 aws s3 sync s3://${S3_BUCKET}/ s3://backup.${S3_BUCKET}/
 grep -lr 'http://'${ORIGIN_URL} ./${ORIGIN_URL}/* | xargs -i sed -i {} -e 's#http://'${ORIGIN_URL}'#https://'${ORIGIN_URL}'#g'
 aws s3 cp --recursive ./${ORIGIN_URL}/ s3://${S3_BUCKET}/
